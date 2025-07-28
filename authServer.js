88254e70cdd6e2b480257d4a78c3004f40dbf886
Emul4Y <emreyasinkocaman07@gmail.com>
2025-07-28 12:10:49 +0300

    bcrypt entegrasyonu

--- authServer.js
+++ authServer.js
@@ -4,39 +4,51 @@ const express = require('express')
 const app = express()
 const jwt = require("jsonwebtoken")
 
-
 app.use(express.json())
 
-let refreshTokens = []
+// Oturum açan her kullanıcının refresh token’ları burada tutulur.
+let refreshTokens = [] 
 
 app.post('/token' , (req , res) => {
 
-    const refreshToken = req.body.token
-    if(refreshToken == null ) return res.sendStatus(401)
-    if(!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+    const refreshToken = req.body.token // token endpoint’i refresh token alır.
+    if(refreshToken == null ) return res.sendStatus(401) // Token yoksa 401
+    if(!refreshTokens.includes(refreshToken)) return res.sendStatus(403) // listede yoksa 403 döner
     
+    // Refresh token doğrulanır.
     jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err , user) => {
         if(err) return res.sendStatus(403)
-        const accessToken = generateAccessToken({ Mail : user.gmail }) 
-        res.json({ accessToken : accessToken})
+        const accessToken = generateAccessToken({ Mail : user.gmail }) // İçindeki kullanıcı bilgisine göre yeni access token üretilir
+        res.json({ accessToken : accessToken}) // Cevap olarak client’a yeni access token gönderilir
 
     })
 })
 
+//  /login endpoint’i e-posta ile oturum açmak içindir (şifre kontrolü yapılmaz, örnek amaçlı ) 
 app.post('/login' , (req ,res) => {
     //Authenticate User
 
     const gmail = req.body.gmail
-    const user = { Mail : gmail}
+    const user = { Mail : gmail}// Mail isimli payload ile JWT token oluşturulacak
 
-    const accessToken = generateAccessToken(user)
+    const accessToken = generateAccessToken(user) //accessToken ve refreshToken üretilir
     const refreshToken = jwt.sign(user , process.env.REFRESH_TOKEN_SECRET)
-    refreshTokens.push(refreshToken)
-    res.json({accessToken: accessToken , refreshToken: refreshToken})
+    refreshTokens.push(refreshToken) // refreshToken listeye kaydedilir
+    res.json({accessToken: accessToken , refreshToken: refreshToken}) //Kullanıcıya her iki token da verilir
 }) 
 
-function generateAccessToken(user) {
+// ACCESS_TOKEN_SECRET ile şifrelenmiş, 1 dakika geçerli access token üretir
+function generateAccessToken(user) { 
     return jwt.sign(user , process.env.ACCESS_TOKEN_SECRET , {expiresIn : '1m'})
 }
 
 app.listen(4000)
+
+/* 
+        --  authServer  (Bu sunucu kullanıcılara giriş işlemi sağlar)   --
+
+- accessToken → server.js dosyasına gönderilip /posts verisine erişmek için kullanılır
+- refreshToken → accessToken süresi dolunca /token ile yenilenebilir
+- Her iki token da .env dosyasındaki secret’lar ile şifrelenmiştir
+
+*/
--- server.js
+++ server.js
@@ -1,26 +1,30 @@
-require('dotenv').config()
+// .env dosyasındaki gizli anahtarları (ACCESS_TOKEN_SECRET) projeye dahil eder.
+require('dotenv').config() 
 
+// Express framework’ünü ve JWT (token doğrulama kütüphanesi) projeye dahil eder.
 const express = require('express')
 const app = express()
 const jwt = require("jsonwebtoken")
+const bcrypt = require('bcrypt')
 
-
+// Gelen JSON formatındaki istekleri anlamasını sağlar (req.body'yi kullanabiliriz).
 app.use(express.json())
 
 const posts = [
     {
         gmail : 'emre@gmail.com',
-        password: '1234'
+        password: bcrypt.hashSync('1234', 10)
     },
     {
         gmail : 'yasin@gmail.com',
-        password: '4321'
+        password: bcrypt.hashSync('4321', 10)
     }
 ]
 
+// authenticateToken middleware’i ile token kontrolü yapılır.
 app.get('/posts', authenticateToken, (req,res) => {
     
-    res.json(posts.filter( post => post.gmail === req.user.Mail))
+    res.json(posts.filter( post => post.gmail === req.user.Mail))//Token geçerliyse, sadece o kullanıcıya ait post'lar döndürülür.
 })
 
 function authenticateToken (req , res , next) {
@@ -30,9 +34,9 @@ function authenticateToken (req , res , next) {
 
     if ( token == null ) return res.sendStatus(401)
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET , (err , user) => {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET , (err , user) => {//Doğruysa, içindeki Mail bilgisini req.user’a atar
         
-        if (err) return res.sendStatus(403)
+        if (err) return res.sendStatus(403)//Token doğrulanır. Hatalıysa 403 döner
 
         req.user = user
         next()
@@ -41,3 +45,11 @@ function authenticateToken (req , res , next) {
 
 }
 app.listen(3000)
+/*    
+        --  server.js (Korunan veri sunucusu (API sunucu) ) -- 
+
+- authServer.js tarafından oluşturulan access token`ı kontrol eder.
+- Doğrulama için .env dosyasındaki ACCESS_TOKEN_SECRET kullanılır.
+- Kullanıcı sadece geçerli access token’la /posts verisine erişebilir
+
+*/ 
